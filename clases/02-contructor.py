211ab4b8a82ca381de6afe0385fbffedf59c3d66
“ariel.duarte” <arielpy@gmail.com>
2024-08-26 11:34:44 -0300

    constructor

--- clases/02-contructor.py
+++ clases/02-contructor.py
@@ -5,3 +5,10 @@ class Perro:
 
     def ladrar(self):
         print(f'{self.nombre} está ladrando')
+
+
+perro1 = Perro('Firulais', 'Pastor Alemán')
+perro2 = Perro('Rex', 'Doberman')
+
+perro1.ladrar()  # Firulais está ladrando
+perro2.ladrar()  # Rex está ladrando
